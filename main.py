30bd41b81e93ee8efde11c8d436be29ac222105b
ray braga <rayzaumbraga@gmail.com>
2024-12-27 16:42:38 +0000

    Atualizacoes

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH} ")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.display.set_caption("Asteroids")
+
+
+    #loop principal do jogo, enquanto estiver jogando (running) = true, quando eu quiser fechar (running) = false
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #tela cor preta
+        screen.fill((0, 0, 0))
+
+        #atualiza a tela
+        pygame.display.flip()
+
+    #feixa o jogo
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
